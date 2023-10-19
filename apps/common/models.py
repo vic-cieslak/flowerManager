7930a8b74f14d2464c252cfdd7e8830b850c12f5
wiktorcie <wiktorcie@gmail.com>
2023-10-19 16:31:52 +0200

    faviconki i teskty

--- apps/common/models.py
+++ apps/common/models.py
@@ -11,11 +11,23 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-# class Kwiat(models.Model):
-#     id    = models.AutoField(primary_key=True)
-#     name  = models.CharField(max_length = 100) 
-#     info  = models.CharField(max_length = 100, default = '')
-#     price = models.IntegerField(blank=True, null=True)
+class Kwiat(models.Model):
+    id                  = models.AutoField(primary_key=True)
+    name                = models.CharField(max_length = 100) 
+    kategorie_i_kolory  = models.CharField(max_length = 50000, default = '')
+    aktywny             = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
 
-#     def __str__(self):
-#         return self.name
+class Kolory(models.Model):
+    id                  = models.AutoField(primary_key=True)
+    name                = models.CharField(max_length = 100) 
+    hex_kolor           = models.CharField(max_length = 100, default = '')
+
+    def __str__(self):
+        return self.name
+
+class Raport(models.Model):
+    id                  = models.AutoField(primary_key=True)
+    data_utworzenia     = models.DateTimeField(auto_now=True)
+    wartosc             = models.CharField(max_length = 500000, default = '')
