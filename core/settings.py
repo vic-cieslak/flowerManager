f48524f0a813175709e3e8da417ce66ab7e900bf
wiktorcie <wiktorcie@gmail.com>
2023-10-20 09:10:51 +0200

    update trusted origin

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ DEBUG = strtobool(os.getenv('DEBUG', "True"))
 
 # Hosts Settings
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com', '0.0.0.0', '165.22.80.96']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085', 'https://rocket-django.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085', 'http://165.22.80.96:5085']
 
 # Used by DEBUG-Toolbar 
 INTERNAL_IPS = [
