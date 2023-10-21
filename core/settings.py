3992c5bcec4f5b312eb7126150445bc96e560e51
wiktorcie <wiktorcie@gmail.com>
2023-10-21 08:46:16 +0200

    add postgres

--- core/settings.py
+++ core/settings.py
@@ -106,18 +106,18 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
     # "default": {
-    #     "ENGINE": "django.db.backends.postgresql",
-    #     "NAME": os.getenv("POSTGRES_DB", "hello"),
-    #     "USER": os.getenv("POSTGRES_USER", "hello"),
-    #     "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
-    #     "HOST": os.getenv("POSTGRES_HOST", "postgres"),
-    #     "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
     # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "hello"),
+        "USER": os.getenv("POSTGRES_USER", "hello"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
+        "HOST": os.getenv("POSTGRES_HOST", "postgres"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
 }
 
 
