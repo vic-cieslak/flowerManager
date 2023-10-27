c51885aff346d648235195489d7443d3b825477a
wiktorcie <wiktorcie@gmail.com>
2023-10-27 15:53:51 +0200

    poprawione formatowanie raportu, kolumny chce / nie chce

--- apps/tables/templatetags/color_tags.py
+++ apps/tables/templatetags/color_tags.py
@@ -3,3 +3,15 @@ from django.template.defaulttags import register
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def items(dictionary):
+    return dictionary.items()
+
+@register.filter
+def dict_values_neq(dictionary, value):
+    return {k: v for k, v in dictionary.items() if v != value}
+
+@register.filter
+def dict_values_eq(dictionary, value):
+    return {k: v for k, v in dictionary.items() if v == value}
