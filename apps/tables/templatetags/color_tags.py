92a503c7d284e3a9bf1d2484cb6e281ba0a834fe
wiktorcie <wiktorcie@gmail.com>
2023-11-05 13:50:39 +0100

    add new tag

--- apps/tables/templatetags/color_tags.py
+++ apps/tables/templatetags/color_tags.py
@@ -15,3 +15,10 @@ def dict_values_neq(dictionary, value):
 @register.filter
 def dict_values_eq(dictionary, value):
     return {k: v for k, v in dictionary.items() if v == value}
+
+@register.filter(name='cut_multiple')
+def cut_multiple(value, arg):
+    """Removes all values of arg from the given string."""
+    for character in arg:
+        value = value.replace(character, '')
+    return value
